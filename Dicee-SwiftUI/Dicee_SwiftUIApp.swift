1df438791a6c0ec914794a266f16c255ba3c798e
Mykhailo Radzievskyi <nn5n.com@gmail.com>
2023-05-08 20:10:02 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mykhailo Radzievskyi on 2023-05-08.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
